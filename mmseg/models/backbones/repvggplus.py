06f6ae367a49853da93f9fb1f2923e93e09be2d6
dingxiaohan <dingxiaohan@megvii.com>
2021-10-22 14:40:13 +0800

    55

--- mmseg/models/backbones/repvggplus.py
+++ mmseg/models/backbones/repvggplus.py
@@ -9,12 +9,15 @@ class SEBlock(nn.Module):
 
     def __init__(self, input_channels, internal_neurons):
         super(SEBlock, self).__init__()
+        self.gap = nn.AdaptiveAvgPool2d(1)
         self.down = nn.Conv2d(in_channels=input_channels, out_channels=internal_neurons, kernel_size=1, stride=1, bias=True)
         self.up = nn.Conv2d(in_channels=internal_neurons, out_channels=input_channels, kernel_size=1, stride=1, bias=True)
         self.input_channels = input_channels
 
+
     def forward(self, inputs):
-        x = F.avg_pool2d(inputs, kernel_size=inputs.size(3))
+        x = self.gap(inputs)
+        # print('gap: ', self.gap)
         x = self.down(x)
         x = F.relu(x)
         x = self.up(x)
